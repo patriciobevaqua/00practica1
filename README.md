7f48378579bea0fcee14fe131d9607605bd1f9eb
patriciobevaqua <patriciobq@gmail.com>
2018-05-04 17:31:38 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# 00practica1
---
+++ miScript.js
@@ -0,0 +1,7 @@
+
+// se suele utilizar algún framework como jquery
+
+alert("hola a todos..... Bienvenidos!!"); //alert es una función que recibe como parametro un string
+
+document.getElementById("tit").style.color = "yellow";
+//document.getElementById("tit").style.width = "80%";
